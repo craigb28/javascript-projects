35ddea8394788b3706e18135bc9897e60d375ccb
Craig Brummer <craigbrummer28@gmail.com>
2024-01-10 16:28:13 -0600

    Exercises completed.

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,46 @@
 console.log("Some Programming Languages:");
 
-console.log("Python\nJavaScript\nJava\nC#\nSwift");
+console.log("\tPython\n\tJavaScript\n\tJava\nC#\nSwift");
+
+console.log(typeof "Hello, World!")
+console.log(typeof 4)
+console.log(typeof "4")
+
+console.log(typeof 4+2+1+3)
+
+console.log("Bruce's beard");
+console.log('Bruce"s beard');
+
+console.log(42, 17, 56, 34, 11, 4.35, 32);
+console.log(3.4, "hello", 45);
+
+
+
+let day = "Thursday";
+console.log(day);
+
+day = "Friday";
+console.log(day);
+
+day = 21;
+console.log(day);
+console.log(typeof day);
+
+const appName = "Get It Done";
+console.log(appName)
+
+console.log (2**2**3*3)
+
+let x = 1;
+x = x + 1;
+
+console.log(x);
+
+x = 1;
+x += 1;
+
+console.log(x);
+
+const input = require('readline-sync');
+
+let name = input.question("Enter your name: ");
