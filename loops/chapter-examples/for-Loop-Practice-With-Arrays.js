69b55d5727b2dd4d58fbe514362149f1bcb1b27c
Craig Brummer <craigbrummer28@gmail.com>
2024-01-22 14:35:19 -0600

    Loop exercises completed

--- loops/chapter-examples/for-Loop-Practice-With-Arrays.js
+++ loops/chapter-examples/for-Loop-Practice-With-Arrays.js
@@ -1,3 +1,9 @@
 // create an array variable containing the names
 
+let names = ["Gary", "Jane", "Justin"]
+
 // write a for loop that prints each name on a different line
+
+for (let index = 0; index < names.length; index++) {
+    console.log(names[index])
+}
