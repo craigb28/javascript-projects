c2f8624eb237306988aed0e6eb91c6ccf619ab96
Craig Brummer <craigbrummer28@gmail.com>
2024-01-22 14:36:15 -0600

    Array studio updated.

--- arrays/studio/multi-dimensional-arrays.js
+++ arrays/studio/multi-dimensional-arrays.js
@@ -5,10 +5,49 @@ let sleepAids = "blankets,pillows,eyepatches,alarm clocks";
 
 //1) Use split to convert the strings into four cabinet arrays. Alphabetize the contents of each cabinet.
 
+let foodCabinet = food.split(",").sort()
+let equipmentCabinet = equipment.split(",").sort()
+let petsCabinet = pets.split(",").sort()
+let sleepAidsCabinet = sleepAids.split(",").sort()
+
+console.log(foodCabinet)
+console.log(equipmentCabinet)
+console.log(petsCabinet)
+console.log(sleepAidsCabinet)
+
+
 //2) Initialize a cargoHold array and add the cabinet arrays to it. Print cargoHold to verify its structure.
 
+let cargoHold = [foodCabinet, equipmentCabinet, petsCabinet, sleepAidsCabinet]
+
+console.log(cargoHold)
+console.log(typeof (cargoHold))
 //3) Query the user to select a cabinet (0 - 3) in the cargoHold.
+
+const input = require('readline-sync');
+
+let cabinetSelection = Number(input.question("Which cabinet would you like to explore? "));
 
 //4) Use bracket notation and a template literal to display the contents of the selected cabinet. If the user entered an invalid number, print an error message.
 
+let itemSelection = input.question("What are item are you looking for? ")
+
+if (cabinetSelection >0 && cabinetSelection <=3) {
+    console.log(`Cabinet ${cabinetSelection} contains: ${cargoHold[cabinetSelection-1]}.`)
+} else {
+    console.log(`Error: There isn't a cabinet with that number.`)
+}
 //5) Modify the code to query the user for BOTH a cabinet in cargoHold AND a particular item. Use the 'includes' method to check if the cabinet contains the selected item, then print “Cabinet ____ DOES/DOES NOT contain ____.”
+
+// let itemSelection = input.question("What are item are you looking for? ")
+
+//  if (cabinetSelection >0 && cabinetSelection <=3 && (cargoHold[cabinetSelection][0,3].includes(itemSelection)))
+//     {
+//     console.log (`Cabinet ${cabinetSelection} does contain ${itemSelection}.`)
+//     }
+// else {
+//     console.log (`Cabinet ${cabinetSelection} does NOT contain ${itemSelection}.`)
+//  }}
+//  else {
+//     console.log(`Error: There isn't a cabinet with that number.`)
+//  }
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -4,8 +4,22 @@ let str = "LaunchCode";
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
 
+let newStr = str.slice(3)+str.slice(0,3)
+
+console.log(newStr)
+
 //Use a template literal to print the original and modified string in a descriptive phrase.
+
+console.log(`${newStr} is Pig-Latin for ${str}`)
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
 
+let numberResp = Number(input.question("Give me a number between 1 and 9. "))
+
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+
+if (numberResp >= 1 && numberResp <= (str.length-1)) {
+    console.log(`Great! ${numberResp} is between 1 and 9! Your new word is ${str.slice(numberResp)+str.slice(0,numberResp)}`)
+} else {
+    console.log(`ERROR: ${numberResp} is not between 1 and 9! ${newStr} is your new word.`)
+}
