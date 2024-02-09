66dae840127a408a65bf3e55ab841daac4cbceca
Craig Brummer <craigbrummer28@gmail.com>
2024-02-08 20:29:58 -0600

    studio done

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,29 @@
 
 let launchcode = {
-  
-}
+    Organization:"nonprofit",
+    executiveDirector:"Jeff",
+    percentageCoolEmployees:100,
+    programsOffered: ["WebDev", "DataAnalysis", "Liftoff"],
+    launchOutput: function(num) {
+        if (num%2===0 && num%3===0){
+            return "LaunchCode!"
+        }
+        else if (num%3===0 && num%5===0){
+            return "Code Rocks!"
+        }
+        else if (num%2 === 0){
+            return "Launch!"
+        }
+        else if (num%3 === 0){
+            return "Code!"
+        }
+        else if (num%5===0){
+            return "Rocks!"
+        }
+        
+    }
+};
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -3,6 +3,34 @@ const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
 
+  test("Object keys match appropriate values", function(){
+    expect(launchcode.Organization).toBe("nonprofit");
+    expect(launchcode.executiveDirector).toBe("Jeff");
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test("List of programs includes WebDev, DataAnalysis, and Liftoff", function(){
+    expect(launchcode.programsOffered.includes("WebDev")).toBe(true);
+    expect(launchcode.programsOffered.includes("DataAnalysis")).toBe(true);
+    expect(launchcode.programsOffered.includes("Liftoff")).toBe(true);
+    expect(launchcode.programsOffered.length).toBe(3);
+  });
+  test("When passed a number that is ONLY divisible by 2, returns Launch!", function (){
+    expect(launchcode.launchOutput(2)).toBe("Launch!");
+  });
+  test("When passed a number that is ONLY divisible by 3, returns Launch!", function (){
+    expect(launchcode.launchOutput(3)).toBe("Code!");
+  });
+  test("When passed a number that is ONLY divisible by 5, returns Rocks!", function (){
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  });
+  test("When passed a number that is divisible by 2 AND 3, returns LaunchCode!", function (){
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  });
+  test("When passed a number that is divisible by 3 AND 5, returns Code Rocks!", function (){
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  });
+
+
   // Write your unit tests here!
   
 });
